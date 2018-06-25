b2225fac855cc6875ad63d329cfdaa964787a6ac
Jim Minter <jminter@redhat.com>
2018-06-25 10:44:31 -0500

    fix go get

--- pkg/helm/helm.go
+++ pkg/helm/helm.go
@@ -1,6 +1,6 @@
 package helm
 
-//go get github.com/go-bindata/go-bindata/go-bindata
+//go:generate go get github.com/go-bindata/go-bindata/go-bindata
 //go:generate go-bindata -nometadata -pkg $GOPACKAGE -prefix data data/...
 //go:generate gofmt -s -l -w bindata.go
 
