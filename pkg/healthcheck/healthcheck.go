43fd8146742e43454d74ad99e0ac94cd2d6cee5b
Jim Minter <jminter@redhat.com>
2018-06-29 17:40:03 -0500

    healthcheck fixes (tolerate timeouts)

--- pkg/healthcheck/healthcheck.go
+++ pkg/healthcheck/healthcheck.go
@@ -6,6 +6,7 @@ import (
 	"crypto/x509"
 	"fmt"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/jim-minter/azure-helm/pkg/api"
@@ -37,6 +38,10 @@ func waitForConsole(ctx context.Context, m *api.Manifest, c *config.Config) erro
 
 	for {
 		resp, err := cli.Do(req)
+		if err, ok := err.(*url.Error); ok && err.Timeout() {
+			time.Sleep(10 * time.Second)
+			continue
+		}
 		if err != nil {
 			return err
 		}
@@ -45,11 +50,9 @@ func waitForConsole(ctx context.Context, m *api.Manifest, c *config.Config) erro
 		case http.StatusOK:
 			return nil
 		case http.StatusBadGateway:
-			// we expect a 502 when not ready yet -- keep looping
+			time.Sleep(10 * time.Second)
 		default:
 			return fmt.Errorf("unexpected error code %d from console", resp.StatusCode)
 		}
-
-		time.Sleep(time.Second)
 	}
 }
