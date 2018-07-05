26a0634b839862175b8feddd5b117af0c0124492
Jim Minter <jminter@redhat.com>
2018-07-05 18:24:16 -0500

    wait for deployments after helm upgrade (needs testing)

--- upgrade.sh
+++ upgrade.sh
@@ -7,6 +7,15 @@ go run cmd/createorupdate/createorupdate.go
 
 KUBECONFIG=aks/admin.kubeconfig helm upgrade $RESOURCEGROUP pkg/helm/chart -f _data/_out/values.yaml >/dev/null
 
+# TODO: verify the kubectl rollout status code below, not convinced that it's
+# working properly
+
+# TODO: when sync runs as an HCP pod (i.e. not in development), hopefully should
+# be able to use helm upgrade --wait here
+for d in master-etcd master-api master-controllers; do
+    KUBECONFIG=aks/admin.kubeconfig kubectl rollout status deployment $d -n $RESOURCEGROUP -w
+done
+
 # TODO: need to apply ARM deployment changes
 
 # will eventually run as an HCP pod, for development run it locally
