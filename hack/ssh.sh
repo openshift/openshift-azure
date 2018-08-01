316db4ce03f1f8623fd01387cb643f959e90986c
Kenny Woodson <kwoodson@redhat.com>
2018-08-01 13:15:26 -0400

    Enable MSI for deployments.

--- hack/ssh.sh
+++ hack/ssh.sh
@@ -11,5 +11,9 @@ ID=$3
 
 IP=$(az vmss list-instance-public-ips -g $RESOURCEGROUP -n $SS --query "[$ID].ipAddress" | tr -d '"')
 
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+    -o LogLevel=QUIET -i _data/_out/id_rsa _data/_out/id_rsa cloud-user@$IP:
+
+
 exec ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
     -o LogLevel=QUIET -i _data/_out/id_rsa cloud-user@$IP
