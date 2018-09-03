4e7dce0b26caeff4e0be0657575638defed1490c
Jim Minter <jminter@redhat.com>
2018-09-03 15:17:27 -0500

    only set public IPs on masters

--- hack/ssh.sh
+++ hack/ssh.sh
@@ -1,15 +1,24 @@
 #!/bin/bash -e
 
-if [[ $# -ne 3 ]]; then
-    echo "usage: $0 resourcegroup ss-master|ss-infra|ss-compute {0,1,2,...}"
+killagent() {
+    [[ -n "$SSH_AGENT_PID" ]] && kill "$SSH_AGENT_PID"
+}
+
+if [[ $# -ne 2 ]]; then
+    echo "usage: $0 resourcegroup {0,1,2}"
     exit 1
 fi
 
-RESOURCEGROUP=$1
-SS=$2
-ID=$3
+RESOURCEGROUP="$1"
+ID="$2"
+shift 2
 
-IP=$(az vmss list-instance-public-ips -g $RESOURCEGROUP -n $SS --query "[$ID].ipAddress" | tr -d '"')
+IP=$(az vmss list-instance-public-ips -g $RESOURCEGROUP -n ss-master --query "[$ID].ipAddress" | tr -d '"')
 
-exec ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-    -o LogLevel=QUIET -i _data/_out/id_rsa cloud-user@$IP
+trap killagent EXIT
+
+eval "$(ssh-agent)"
+ssh-add _data/_out/id_rsa 2>/dev/null
+
+ssh -A -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+    -o LogLevel=QUIET "$@" cloud-user@$IP
