08d9d089a2ec654a18e1087110de539e3d086e15
Jim Minter <jminter@redhat.com>
2018-09-12 19:29:26 -0500

    allow ssh into arbitrary clusters, stop requiring resourcegroup on delete

--- hack/delete.sh
+++ hack/delete.sh
@@ -14,18 +14,12 @@ if [[ -z "$DNS_RESOURCEGROUP" ]]; then
     exit 1
 fi
 
-if [[ ! -e _data/manifest.yaml ]]; then
-    echo error: _data/manifest.yaml must exist
+if [[ ! -e _data/containerservice.yaml ]]; then
+    echo error: _data/containerservice.yaml must exist
     exit 1
 fi
 
-if [[ $# -ne 1 ]]; then
-    echo usage: $0 resourcegroup
-    exit 1
-fi
-
-RESOURCEGROUP=$1
-PUBLICHOSTNAME=$(awk '/^  publicHostname:/ { print $2 }' <_data/manifest.yaml)
+RESOURCEGROUP=$(awk '/^    resourceGroup:/ { print $2 }' <_data/containerservice.yaml)
 
 hack/dns.sh zone-delete $RESOURCEGROUP
 
