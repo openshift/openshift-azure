8dfdc03886f356cadb8d6924fdf649e919f6f638
Kenny Woodson <kwoodson@redhat.com>
2018-10-01 05:45:31 -0400

    Adding tests for osa container images. (#520)
    * Adding tests for osa container images.
    
    * Use format string.
    
    * Updated test to check image config.
    
    * Adding OS type registry check.
    
    * Parsing node-config.yaml for image format

--- test/e2e/osa_test.go
+++ test/e2e/osa_test.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/ghodss/yaml"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -14,6 +16,8 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/openshift/openshift-azure/pkg/jsonpath"
 )
 
 type testClient struct {
@@ -82,5 +86,38 @@ var _ = Describe("Openshift on Azure e2e tests", func() {
 			}
 		})
 		Expect(err).ToNot(HaveOccurred())
+	})
+
+	It("should run the correct image", func() {
+		pods, err := c.kc.CoreV1().Pods("").List(metav1.ListOptions{})
+		Expect(err).ToNot(HaveOccurred())
+		// e2e check should ensure that no reg-aws images are running on box
+		for _, pod := range pods.Items {
+			for _, cntr := range pod.Spec.Containers {
+				Expect(strings.HasPrefix(cntr.Image, "registry.reg-aws.openshift.com/")).ToNot(BeTrue())
+			}
+		}
+		// fetch master-000000 and determine the OS type
+		master0, _ := c.kc.CoreV1().Nodes().Get("master-000000", metav1.GetOptions{})
+		Expect(err).ToNot(HaveOccurred())
+
+		// set registryPrefix to appropriate string based upon master's OS type
+		var registryPrefix string
+		if strings.HasPrefix(master0.Status.NodeInfo.OSImage, "Red Hat Enterprise") {
+			registryPrefix = "registry.access.redhat.com/openshift3/ose-"
+		} else {
+			registryPrefix = "docker.io/openshift/origin-"
+		}
+
+		// Check all Configmaps for image format matches master's OS type
+		// format: registry.access.redhat.com/openshift3/ose-${component}:${version}
+		maps, err := c.kc.CoreV1().ConfigMaps("openshift-node").List(metav1.ListOptions{})
+		Expect(err).ToNot(HaveOccurred())
+		var nodeConfig map[string]interface{}
+		for _, cm := range maps.Items {
+			err = yaml.Unmarshal([]byte(cm.Data["node-config.yaml"]), &nodeConfig)
+			format := jsonpath.MustCompile("$.imageConfig.format").MustGetString(nodeConfig)
+			Expect(strings.HasPrefix(format, registryPrefix)).To(BeTrue())
+		}
 	})
 })
