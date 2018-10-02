8853e536c88809d98bfeede627553f093dc6aa28
Michalis Kargakis <kargakis@protonmail.ch>
2018-10-02 14:07:52 +0200

    Update hack/e2e.sh to print ginkgo output

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -11,11 +11,9 @@ fqdn=$(hack/config.sh get-config $RESOURCEGROUP | jq -r .properties.fqdn)
 export KUBECONFIG=_data/_out/osadmin.kubeconfig
 # oc login is going to create the osadmin.kubeconfig for us
 oc login $fqdn --username osadmin --password $password --insecure-skip-tls-verify=true
-oc new-project e2e-end-user-test-root
-go test ./test/e2e -ginkgo.focus="\[EndUser\]" -tags e2e -kubeconfig ../../_data/_out/osadmin.kubeconfig
-oc delete project e2e-end-user-test-root
+go test ./test/e2e -test.v -ginkgo.v -ginkgo.focus="\[EndUser\]" -tags e2e -kubeconfig ../../_data/_out/osadmin.kubeconfig
 oc logout
 
 echo "Running azure cluster reader e2e tests"
 (hack/config.sh get-config $RESOURCEGROUP | jq -r .config.azureClusterReaderKubeconfig | base64 -d) > _data/_out/azure-cluster-reader.kubeconfig
-go test ./test/e2e -ginkgo.focus="\[AzureClusterReader\]" -tags e2e -kubeconfig ../../_data/_out/azure-cluster-reader.kubeconfig
+go test ./test/e2e -test.v -ginkgo.v -ginkgo.focus="\[AzureClusterReader\]" -tags e2e -kubeconfig ../../_data/_out/azure-cluster-reader.kubeconfig
