d9a265c14611ef5c24240d9e4b498643741c8c07
Jorge Gallegos <jgallego@redhat.com>
2018-11-05 14:14:20 -0600

    Comply with unit test

--- pkg/api/default.go
+++ pkg/api/default.go
@@ -1,28 +1,35 @@
 package api
 
-import "strings"
-
-// TODO: calculate the reserved resource sizes based on instance resources
-func getAgentPoolProfile(role string) map[ReservedResource]string {
+// TODO: calculate the reserved resource sizes based on instance resources?
+func getAgentPoolProfile(role AgentPoolProfileRole, small bool) map[ReservedResource]string {
 	// default resources
-	var reservedResources = map[ReservedResource]string{
-		SystemReserved: "cpu=200m,memory=512Mi",
+	var baseline, master string
+	if small {
+		baseline = "cpu=200m,memory=512Mi"
+		master = "cpu=500m,memory=1Gi"
+	} else {
+		baseline = "cpu=500m,memory=512Mi"
+		master = "cpu=1000m,memory=1Gi"
 	}
-	if strings.ToLower(role) == "master" {
-		// bump up master's system reserves
-		reservedResources[SystemReserved] = "cpu=500m,memory=1Gi"
+	var reservedResources = map[ReservedResource]string{
+		SystemReserved: baseline,
+	}
+	if role == AgentPoolProfileRoleMaster {
+		reservedResources[SystemReserved] = master
 	} else {
 		// we only set the kube reserved resources for non-master roles
-		reservedResources[KubeReserved] = "cpu=200m,memory=512Mi"
+		reservedResources[KubeReserved] = baseline
 	}
 	return reservedResources
 }
 
-func getAgentPoolProfiles() map[AgentPoolProfileRole]map[ReservedResource]string {
+func getAgentPoolProfiles(size VMSize) map[AgentPoolProfileRole]map[ReservedResource]string {
+	// The smallest sizes are used for QA, not intended for production usage
+	var small = size == StandardD2sV3 || size == StandardE2sV3
 	var agentPoolProfiles = map[AgentPoolProfileRole]map[ReservedResource]string{
-		AgentPoolProfileRoleMaster:  getAgentPoolProfile("master"),
-		AgentPoolProfileRoleInfra:   getAgentPoolProfile("infra"),
-		AgentPoolProfileRoleCompute: getAgentPoolProfile("compute"),
+		AgentPoolProfileRoleMaster:  getAgentPoolProfile(AgentPoolProfileRoleMaster, small),
+		AgentPoolProfileRoleInfra:   getAgentPoolProfile(AgentPoolProfileRoleInfra, small),
+		AgentPoolProfileRoleCompute: getAgentPoolProfile(AgentPoolProfileRoleCompute, small),
 	}
 	return agentPoolProfiles
 }
@@ -30,28 +37,28 @@ func getAgentPoolProfiles() map[AgentPoolProfileRole]map[ReservedResource]string
 // DefaultVMSizeKubeArguments defines default values of kube-arguments based on the VM size
 var DefaultVMSizeKubeArguments = map[VMSize]map[AgentPoolProfileRole]map[ReservedResource]string{
 	// standard instances
-	StandardD2sV3:  getAgentPoolProfiles(),
-	StandardD4sV3:  getAgentPoolProfiles(),
-	StandardD8sV3:  getAgentPoolProfiles(),
-	StandardD16sV3: getAgentPoolProfiles(),
-	StandardD32sV3: getAgentPoolProfiles(),
-	StandardD64sV3: getAgentPoolProfiles(),
+	StandardD2sV3:  getAgentPoolProfiles(StandardD2sV3),
+	StandardD4sV3:  getAgentPoolProfiles(StandardD4sV3),
+	StandardD8sV3:  getAgentPoolProfiles(StandardD8sV3),
+	StandardD16sV3: getAgentPoolProfiles(StandardD16sV3),
+	StandardD32sV3: getAgentPoolProfiles(StandardD32sV3),
+	StandardD64sV3: getAgentPoolProfiles(StandardD64sV3),
 
 	// memory optimized instances
-	StandardE2sV3:  getAgentPoolProfiles(),
-	StandardE4sV3:  getAgentPoolProfiles(),
-	StandardE8sV3:  getAgentPoolProfiles(),
-	StandardE16sV3: getAgentPoolProfiles(),
-	StandardE20sV3: getAgentPoolProfiles(),
-	StandardE32sV3: getAgentPoolProfiles(),
-	StandardE64sV3: getAgentPoolProfiles(),
+	StandardE2sV3:  getAgentPoolProfiles(StandardE2sV3),
+	StandardE4sV3:  getAgentPoolProfiles(StandardE4sV3),
+	StandardE8sV3:  getAgentPoolProfiles(StandardE8sV3),
+	StandardE16sV3: getAgentPoolProfiles(StandardE16sV3),
+	StandardE20sV3: getAgentPoolProfiles(StandardE20sV3),
+	StandardE32sV3: getAgentPoolProfiles(StandardE32sV3),
+	StandardE64sV3: getAgentPoolProfiles(StandardE64sV3),
 
 	// storage optimized instances
-	StandardL4s:  getAgentPoolProfiles(),
-	StandardL8s:  getAgentPoolProfiles(),
-	StandardL16s: getAgentPoolProfiles(),
-	StandardL32s: getAgentPoolProfiles(),
-	StandardL64s: getAgentPoolProfiles(),
+	StandardL4s:  getAgentPoolProfiles(StandardL4s),
+	StandardL8s:  getAgentPoolProfiles(StandardL8s),
+	StandardL16s: getAgentPoolProfiles(StandardL16s),
+	StandardL32s: getAgentPoolProfiles(StandardL32s),
+	StandardL64s: getAgentPoolProfiles(StandardL64s),
 }
 
 // AzureLocations defines a) known regions where we permit OSA to be deployed,
