3babda4a60013ac0e4398eed90ed96542f895331
Jim Minter <jminter@redhat.com>
2018-11-30 08:21:07 -0600

    add prometheus federate->statsd forwarder

---
+++ cmd/metricsbridge/metricsbridge.go
@@ -0,0 +1,234 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/ghodss/yaml"
+	"github.com/prometheus/client_model/go"
+	"github.com/prometheus/common/expfmt"
+	"github.com/sirupsen/logrus"
+
+	"github.com/openshift/openshift-azure/pkg/util/log"
+	"github.com/openshift/openshift-azure/pkg/util/statsd"
+)
+
+var (
+	logLevel  = flag.String("loglevel", "Debug", "Valid values are Debug, Info, Warning, Error")
+	gitCommit = "unknown"
+)
+
+type config struct {
+	Interval                   time.Duration `json:"intervalNanoseconds,omitempty"`
+	PrometheusFederateEndpoint string        `json:"prometheusFederateEndpoint,omitempty"`
+	StatsdEndpoint             string        `json:"statsdEndpoint,omitempty"`
+
+	Series []string `json:"series,omitempty"`
+
+	Namespace string `json:"namespace,omitempty"`
+	Region    string `json:"region,omitempty"`
+
+	Token              string `json:"token,omitempty"`
+	InsecureSkipVerify bool   `json:"insecureSkipVerify,omitempty"`
+
+	log     *logrus.Entry
+	rootCAs *x509.CertPool
+	http    *http.Client
+	statsd  *statsd.Client
+}
+
+func (c *config) load(path string) error {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	if err := yaml.Unmarshal(b, &c); err != nil {
+		return err
+	}
+
+	b, err = ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt")
+	switch {
+	case os.IsNotExist(err):
+	case err != nil:
+		return err
+	default:
+		c.rootCAs = x509.NewCertPool()
+		c.rootCAs.AppendCertsFromPEM(b)
+	}
+
+	b, err = ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/token")
+	switch {
+	case os.IsNotExist(err):
+	case err != nil:
+		return err
+	default:
+		c.Token = string(b)
+	}
+
+	return nil
+}
+
+func (c *config) validate() (errs []error) {
+	if c.Interval < time.Second {
+		errs = append(errs, fmt.Errorf("intervalNanoseconds %q too small", c.Interval))
+	}
+	if _, err := url.Parse(c.PrometheusFederateEndpoint); err != nil {
+		errs = append(errs, fmt.Errorf("prometheusFederateEndpoint: %s", err))
+	}
+	if _, err := net.ResolveUDPAddr("udp", c.StatsdEndpoint); err != nil {
+		errs = append(errs, fmt.Errorf("statsdEndpoint: %s", err))
+	}
+	if len(c.Series) == 0 {
+		errs = append(errs, fmt.Errorf("must configure at least one series"))
+	}
+
+	return
+}
+
+func (c *config) init() error {
+	var err error
+
+	c.statsd, err = statsd.NewClient(c.log, c.StatsdEndpoint)
+	if err != nil {
+		return err
+	}
+
+	c.http = &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:            c.rootCAs,
+				InsecureSkipVerify: c.InsecureSkipVerify,
+			},
+		},
+	}
+
+	return nil
+}
+
+func run(log *logrus.Entry, configpath string) error {
+	c := &config{log: log}
+
+	if err := c.load(configpath); err != nil {
+		return err
+	}
+
+	if errs := c.validate(); len(errs) > 0 {
+		var sb strings.Builder
+		for _, err := range errs {
+			sb.WriteString(err.Error())
+			sb.WriteByte('\n')
+		}
+		return errors.New(sb.String())
+	}
+
+	if err := c.init(); err != nil {
+		return err
+	}
+
+	return c.run()
+}
+
+func (c *config) run() error {
+	prometheusURL, err := url.Parse(c.PrometheusFederateEndpoint)
+	if err != nil {
+		return err
+	}
+
+	v := url.Values{}
+	for _, s := range c.Series {
+		v.Add("match[]", s)
+	}
+	prometheusURL.RawQuery = v.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, prometheusURL.String(), nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Authorization", "Bearer "+c.Token)
+	req.Header.Add("Accept", string(expfmt.FmtText))
+
+	t := time.NewTicker(c.Interval)
+	defer t.Stop()
+
+	for {
+		if err := c.runOnce(req); err != nil {
+			c.log.Warn(err)
+		}
+		<-t.C
+	}
+}
+
+func (c *config) runOnce(req *http.Request) error {
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("prometheus returned status code %d", resp.StatusCode)
+	}
+
+	d := expfmt.NewDecoder(resp.Body, expfmt.FmtText)
+
+	for {
+		var family io_prometheus_client.MetricFamily
+
+		err = d.Decode(&family)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		for _, m := range family.Metric {
+			gauge := &statsd.Gauge{
+				Namespace: c.Namespace,
+				Metric:    *family.Name,
+				Dims: map[string]string{
+					"Region":       c.Region,
+					"UnderlayName": "",
+				},
+				Value: *m.Untyped.Value,
+			}
+			for _, label := range m.Label {
+				gauge.Dims[*label.Name] = *label.Value
+			}
+			if err = c.statsd.Write(gauge); err != nil {
+				return err
+			}
+		}
+	}
+
+	return c.statsd.Flush()
+}
+
+func main() {
+	flag.Parse()
+	logrus.SetLevel(log.SanitizeLogLevel(*logLevel))
+	logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+
+	log := logrus.NewEntry(logrus.StandardLogger())
+	log.Printf("metricsbridge starting, git commit %s", gitCommit)
+
+	if len(os.Args) != 2 {
+		log.Fatalf("usage: %s config.yaml", os.Args[0])
+	}
+
+	if err := run(log, os.Args[1]); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ pkg/util/statsd/statsd.go
@@ -0,0 +1,125 @@
+package statsd
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"syscall"
+
+	"github.com/sirupsen/logrus"
+)
+
+// Gauge represents a statsd gauge
+type Gauge struct {
+	Namespace string
+	Metric    string
+	Dims      map[string]string
+	Value     float64 `json:"-"`
+}
+
+func (g *Gauge) marshal() ([]byte, error) {
+	buf := &bytes.Buffer{}
+
+	e := json.NewEncoder(buf)
+	err := e.Encode(g)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = fmt.Fprintf(buf, ":%f|g\n", g.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+// Client is a buffering statsd client
+type Client struct {
+	log        *logrus.Entry
+	conn       net.Conn
+	buf        bytes.Buffer
+	maxPayload int
+}
+
+// NewClient returns a new client
+func NewClient(log *logrus.Entry, address string) (*Client, error) {
+	conn, err := net.Dial("udp", address)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Client{log: log, conn: conn}
+
+	if err := c.setMaxPayload(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// Flush flushes the internal buffer
+func (c *Client) Flush() error {
+	_, err := c.buf.WriteTo(c.conn)
+	if err, ok := err.(*net.OpError); ok {
+		if err, ok := err.Err.(*os.SyscallError); ok {
+			if err.Err == syscall.EMSGSIZE {
+				err := c.setMaxPayload()
+				if err != nil {
+					c.log.Warn(err)
+				}
+				c.buf.Reset()
+			}
+		}
+	}
+
+	return err
+}
+
+// Write writes a gauge to the internal buffer and possibly flushes
+func (c *Client) Write(g *Gauge) error {
+	b, err := g.marshal()
+	if err != nil {
+		return err
+	}
+
+	if c.buf.Len()+len(b) > c.maxPayload {
+		if err := c.Flush(); err != nil {
+			return err
+		}
+	}
+
+	_, err = c.buf.Write(b)
+	return err
+}
+
+// Close flushes the internal buffer and closes the connection
+func (c *Client) Close() error {
+	if err := c.Flush(); err != nil {
+		return err
+	}
+
+	return c.conn.Close()
+}
+
+func (c *Client) setMaxPayload() error {
+	f, err := c.conn.(*net.UDPConn).File()
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	fd := int(f.Fd())
+
+	mtu, err := syscall.GetsockoptInt(fd, syscall.IPPROTO_IP, syscall.IP_MTU)
+	if err != nil {
+		return err
+	}
+
+	c.maxPayload = mtu - 28 // len(typical IP header) + len(UDP header)
+	c.log.Infof("set maxPayload to %d", c.maxPayload)
+
+	return nil
+}
