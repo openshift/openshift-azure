2fca6bea21aa45435da415956c6f2a96d8dab66a
Michalis Kargakis <kargakis@protonmail.ch>
2018-12-18 10:45:34 +0100

    Trap killing process on 8080

--- hack/create.sh
+++ hack/create.sh
@@ -16,7 +16,10 @@ USE_PROD_FLAG="-use-prod=false"
 if [[ -n "$TEST_IN_PRODUCTION" ]]; then
     USE_PROD_FLAG="-use-prod=true"
 else
-	hack/fakerp.sh $RESOURCEGROUP &
+    go generate ./...
+    go run cmd/fakerp/main.go &
 fi
+
+trap 'set +ex; return_id=$?; kill $(lsof -t -i :8080); wait; exit $return_id' EXIT
 
 go run cmd/createorupdate/createorupdate.go $USE_PROD_FLAG
