31dd3d9160b2ee89d6020d858881dfb5fab4ce00
Angus Salkeld <asalkeld@redhat.com>
2018-12-20 11:41:27 +1000

    Add aad group controller to azure-controllers

---
+++ pkg/controllers/customeradmin/group_mapping.go
@@ -0,0 +1,79 @@
+package customeradmin
+
+import (
+	"context"
+	"reflect"
+	"sort"
+
+	"github.com/Azure/azure-sdk-for-go/services/graphrbac/1.6/graphrbac"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/openshift/api/user/v1"
+	"github.com/sirupsen/logrus"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/openshift/openshift-azure/pkg/api"
+)
+
+// fromMSGraphGroup syncs the values from the given aad group into kubeGroup
+// it returns a boolean "changed" that tells the call if an update is required
+func fromMSGraphGroup(log *logrus.Entry, kubeGroup *v1.Group, kubeGroupName string, msGroupMembers []graphrbac.User) (*v1.Group, bool) {
+	var g *v1.Group
+	if kubeGroup == nil {
+		g = &v1.Group{
+			ObjectMeta: meta_v1.ObjectMeta{
+				Name: kubeGroupName,
+			},
+			Users: []string{},
+		}
+	} else {
+		g = kubeGroup.DeepCopy()
+	}
+	g.Users = []string{}
+	for _, user := range msGroupMembers {
+		g.Users = append(g.Users, *user.UserPrincipalName)
+	}
+	sort.Strings(g.Users)
+	return g, !reflect.DeepEqual(kubeGroup, g)
+}
+
+// https://docs.microsoft.com/en-us/graph/api/group-list
+// To list AAD groups, this code needs to have the clientID
+// of an application with the following permissions:
+// API: Microsoft Graph
+//   Application permissions:
+//      Read all groups
+func newAADGroupsClient(config api.AADIdentityProvider) (*graphrbac.GroupsClient, error) {
+	c := auth.NewClientCredentialsConfig(config.ClientID, config.Secret, config.TenantID)
+	c.Resource = azure.PublicCloud.GraphEndpoint
+
+	authorizer, err := c.Authorizer()
+	if err != nil {
+		return nil, err
+	}
+
+	gc := graphrbac.NewGroupsClient(config.TenantID)
+	gc.Authorizer = authorizer
+	return &gc, nil
+}
+
+func getAADGroupMembers(gc *graphrbac.GroupsClient, groupID string) ([]graphrbac.User, error) {
+	users, err := gc.GetGroupMembers(context.Background(), groupID)
+	if err != nil {
+		return nil, err
+	}
+	members := []graphrbac.User{}
+	for users.NotDone() {
+		for _, bdo := range users.Values() {
+			user, isUser := bdo.AsUser()
+			if isUser {
+				members = append(members, *user)
+			}
+		}
+
+		if err = users.Next(); err != nil {
+			return nil, err
+		}
+	}
+	return members, nil
+}
---
+++ pkg/controllers/customeradmin/group_mapping_test.go
@@ -0,0 +1,133 @@
+package customeradmin
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/Azure/azure-sdk-for-go/services/graphrbac/1.6/graphrbac"
+	"github.com/Azure/go-autorest/autorest/to"
+	"github.com/openshift/api/user/v1"
+	"github.com/sirupsen/logrus"
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestFromMSGraphGroup(t *testing.T) {
+	tests := []struct {
+		name           string
+		kubeGroup      *v1.Group
+		aadGroupID     string
+		kubeGroupName  string
+		msGroupMembers []graphrbac.User
+		want           *v1.Group
+		want1          bool
+	}{
+		{
+			name:          "default group (no aad group)",
+			want1:         true,
+			kubeGroupName: osaCustomerAdmins,
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{},
+			},
+		},
+		{
+			name:          "create new group",
+			want1:         true,
+			kubeGroupName: osaCustomerAdmins,
+			msGroupMembers: []graphrbac.User{
+				{
+					UserPrincipalName: to.StringPtr("foo@somewhere.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"foo@somewhere.com"},
+			},
+		},
+		{
+			name:          "no change",
+			kubeGroupName: osaCustomerAdmins,
+			kubeGroup: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"foo@somewhere.com"},
+			},
+			msGroupMembers: []graphrbac.User{
+				{
+					UserPrincipalName: to.StringPtr("foo@somewhere.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"foo@somewhere.com"},
+			},
+		},
+		{
+			name:          "add to membership",
+			kubeGroupName: osaCustomerAdmins,
+			want1:         true,
+			kubeGroup: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"foo@somewhere.com"},
+			},
+			msGroupMembers: []graphrbac.User{
+				{
+					UserPrincipalName: to.StringPtr("foo@somewhere.com"),
+				},
+				{
+					UserPrincipalName: to.StringPtr("tim@somewhere.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"foo@somewhere.com", "tim@somewhere.com"},
+			},
+		},
+		{
+			name:          "remove from membership",
+			kubeGroupName: osaCustomerAdmins,
+			want1:         true,
+			kubeGroup: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"foo@somewhere.com", "tim@somewhere.com"},
+			},
+			msGroupMembers: []graphrbac.User{
+				{
+					UserPrincipalName: to.StringPtr("foo@somewhere.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"foo@somewhere.com"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			log := logrus.NewEntry(logrus.StandardLogger()).WithField("test", tt.name)
+			got, got1 := fromMSGraphGroup(log, tt.kubeGroup, tt.kubeGroupName, tt.msGroupMembers)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("fromMSGraphGroup() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("fromMSGraphGroup() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
