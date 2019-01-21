3abe0ca0756c820022d39140b0b7e44ea1b45038
Michalis Kargakis <kargakis@protonmail.ch>
2019-01-21 17:19:22 +0100

    Revert "Remove reliance on lsof"
    This reverts commit f07fb7bae80010571625283343dbc057a6ad86a3.

--- hack/upgrade.sh
+++ hack/upgrade.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -ex
 
-cleanup() {
-    kill $(jobs -p) &>/dev/null || true
-    wait
-}
-
-trap cleanup EXIT
-
 if [[ $# -eq 0 && ! -e _data/containerservice.yaml ]]; then
     echo error: _data/containerservice.yaml must exist
     exit 1
@@ -27,5 +20,7 @@ fi
 if [[ -n "$ADMIN_MANIFEST" ]]; then
     ADMIN_MANIFEST="-admin-manifest=$ADMIN_MANIFEST"
 fi
+
+trap 'return_id=$?; set +ex; kill $(lsof -t -i :8080); wait $(lsof -t -i :8080); exit $return_id' EXIT
 
 go run cmd/createorupdate/createorupdate.go -timeout 1h ${TEST_IN_PRODUCTION:-} ${ADMIN_MANIFEST:-}
