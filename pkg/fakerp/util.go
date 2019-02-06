d6ba2ae9bbac1274bf1871cb5c6377e5261718d8
OpenShift Merge Robot <openshift-merge-robot@users.noreply.github.com>
2019-02-06 08:30:06 +0100

    Merge pull request #1099 from charlesakalugwu/refactor-fakerp
    Refactor fakerp package

--- pkg/fakerp/util.go
+++ pkg/fakerp/util.go
@@ -2,7 +2,9 @@ package fakerp
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 func (s *Server) badRequest(w http.ResponseWriter, msg string) {
@@ -15,4 +17,12 @@ func (s *Server) internalError(w http.ResponseWriter, msg string) {
 	resp := fmt.Sprintf("500 Internal Error: %s", msg)
 	s.log.Debug(resp)
 	http.Error(w, resp, http.StatusInternalServerError)
+}
+
+func readBlobName(req *http.Request) (string, error) {
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read request body: %v", err)
+	}
+	return strings.Trim(string(data), "\""), nil
 }
