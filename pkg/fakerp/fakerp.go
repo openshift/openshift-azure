b0fe69be2113babb2cf73c63a0f404760d038a5e
Angus Salkeld <asalkeld@redhat.com>
2019-02-13 11:19:15 +1000

    Revert "Allow the sync pod to be run locally for development"
    This reverts commit 76e6c21cc539fd895511096cd56965c94193d35e.

--- pkg/fakerp/fakerp.go
+++ pkg/fakerp/fakerp.go
@@ -244,17 +244,6 @@ func writeHelpers(cs *api.OpenShiftManagedCluster) error {
 	if err != nil {
 		return err
 	}
-
-	b, err := config.Derived.ClusterConfig(cs)
-	if err != nil {
-		return err
-	}
-
-	err = ioutil.WriteFile(filepath.Join(dataDir, "_out/containerservice.json"), b, 0600)
-	if err != nil {
-		return err
-	}
-
 	// ensure both the new key and the old key are on disk so
 	// you can SSH in regardless of the state of a VM after an update
 	if _, err = os.Stat(filepath.Join(dataDir, "_out/id_rsa")); err == nil {
@@ -267,7 +256,7 @@ func writeHelpers(cs *api.OpenShiftManagedCluster) error {
 			return err
 		}
 	}
-	b, err = config.Derived.CloudProviderConf(cs)
+	b, err := config.Derived.CloudProviderConf(cs)
 	if err != nil {
 		return err
 	}
