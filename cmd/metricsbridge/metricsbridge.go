4dd734ad50e149be0f7d3ddeac5a1778a5af6507
Leszek Jakubowski <makdaam@gmail.com>
2019-03-05 15:21:06 +0100

    Revert "metricsbridge - adding an option to calculate rate of a metric"

--- cmd/metricsbridge/metricsbridge.go
+++ cmd/metricsbridge/metricsbridge.go
@@ -54,9 +54,8 @@ type config struct {
 	StatsdSocket       string        `json:"statsdSocket,omitempty"`
 
 	Queries []struct {
-		Name          string `json:"name,omitempty"`
-		Query         string `json:"query,omitempty"`
-		CalculateRate bool   `json:"calculate_rate,omitempty"`
+		Name  string `json:"name,omitempty"`
+		Query string `json:"query,omitempty"`
 	} `json:"queries,omitempty"`
 
 	Account   string `json:"account,omitempty"`
@@ -214,14 +213,7 @@ func (c *config) runOnce(ctx context.Context) error {
 	var metricsCount int
 
 	for _, query := range c.Queries {
-		var prometheusQuery string
-		if query.CalculateRate {
-			//query for rate of change
-			prometheusQuery = fmt.Sprintf("(%s - %s offset 1m) or (%s unless %s offset 1m)", query.Query, query.Query, query.Query, query.Query)
-		} else {
-			prometheusQuery = query.Query
-		}
-		value, err := c.prometheus.Query(ctx, prometheusQuery, time.Time{})
+		value, err := c.prometheus.Query(ctx, query.Query, time.Time{})
 		if err != nil {
 			return err
 		}
