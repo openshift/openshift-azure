bbff628a76108522a440ddce6e59a79c52e27255
Jim Minter <jminter@redhat.com>
2019-03-05 18:30:01 -0600

    standardise validation messages

--- pkg/api/validate/plugin.go
+++ pkg/api/validate/plugin.go
@@ -18,30 +18,30 @@ func NewPluginAPIValidator() *PluginAPIValidator {
 // Validate validates an Plugin API external template/config struct
 func (v *PluginAPIValidator) Validate(c *pluginapi.Config) (errs []error) {
 	if c == nil {
-		errs = append(errs, fmt.Errorf("Config cannot be nil"))
+		errs = append(errs, fmt.Errorf("config cannot be nil"))
 		return
 	}
 
 	if !rxClusterVersion.MatchString(c.ClusterVersion) {
-		errs = append(errs, fmt.Errorf("invalid ClusterVersion %q", c.ClusterVersion))
+		errs = append(errs, fmt.Errorf("invalid clusterVersion %q", c.ClusterVersion))
 	}
 
 	if c.ImageOffer != "osa" {
-		errs = append(errs, fmt.Errorf("imageOffer should be osa"))
+		errs = append(errs, fmt.Errorf("invalid imageOffer %q", c.ImageOffer))
 	}
 
 	if c.ImagePublisher != "redhat" {
-		errs = append(errs, fmt.Errorf("imagePublisher should be redhat"))
+		errs = append(errs, fmt.Errorf("invalid imagePublisher %q", c.ImagePublisher))
 	}
 
 	switch c.ImageSKU {
 	case "osa_311":
 	default:
-		errs = append(errs, fmt.Errorf("invalid ImageSKU %q", c.ImageSKU))
+		errs = append(errs, fmt.Errorf("invalid imageSKU %q", c.ImageSKU))
 	}
 
 	if !rxImageVersion.MatchString(c.ImageVersion) {
-		errs = append(errs, fmt.Errorf("invalid ImageVersion %q", c.ImageVersion))
+		errs = append(errs, fmt.Errorf("invalid imageVersion %q", c.ImageVersion))
 	}
 
 	for _, prefix := range c.SSHSourceAddressPrefixes {
@@ -55,35 +55,35 @@ func (v *PluginAPIValidator) Validate(c *pluginapi.Config) (errs []error) {
 	errs = append(errs, v.validateImageConfig(&c.Images)...)
 
 	if c.GenevaLoggingSector == "" {
-		errs = append(errs, fmt.Errorf("genevaLoggingSector cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaLoggingSector %q", c.GenevaLoggingSector))
 	}
 
 	if c.GenevaLoggingAccount == "" {
-		errs = append(errs, fmt.Errorf("genevaLoggingAccount cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaLoggingAccount %q", c.GenevaLoggingAccount))
 	}
 
 	if c.GenevaLoggingNamespace == "" {
-		errs = append(errs, fmt.Errorf("genevaLoggingNamespace cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaLoggingNamespace %q", c.GenevaLoggingNamespace))
 	}
 
 	if c.GenevaLoggingControlPlaneAccount == "" {
-		errs = append(errs, fmt.Errorf("genevaLoggingControlPlaneAccount cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaLoggingControlPlaneAccount %q", c.GenevaLoggingControlPlaneAccount))
 	}
 
 	if c.GenevaLoggingControlPlaneEnvironment == "" {
-		errs = append(errs, fmt.Errorf("genevaLoggingControlPlaneEnvironment cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaLoggingControlPlaneEnvironment %q", c.GenevaLoggingControlPlaneEnvironment))
 	}
 
 	if c.GenevaLoggingControlPlaneRegion == "" {
-		errs = append(errs, fmt.Errorf("genevaLoggingControlPlaneRegion cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaLoggingControlPlaneRegion %q", c.GenevaLoggingControlPlaneRegion))
 	}
 
 	if c.GenevaMetricsAccount == "" {
-		errs = append(errs, fmt.Errorf("genevaMetricsAccount cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaMetricsAccount %q", c.GenevaMetricsAccount))
 	}
 
 	if c.GenevaMetricsEndpoint == "" {
-		errs = append(errs, fmt.Errorf("genevaMetricsEndpoint cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid genevaMetricsEndpoint %q", c.GenevaMetricsEndpoint))
 	}
 
 	return
@@ -96,135 +96,135 @@ func (v *PluginAPIValidator) validateImageConfig(i *pluginapi.ImageConfig) (errs
 	}
 
 	if i.Format == "" {
-		errs = append(errs, fmt.Errorf("images.Format cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.format %q", i.Format))
 	}
 
 	if i.ClusterMonitoringOperator == "" {
-		errs = append(errs, fmt.Errorf("images.ClusterMonitoringOperator cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.clusterMonitoringOperator %q", i.ClusterMonitoringOperator))
 	}
 
 	if i.AzureControllers == "" {
-		errs = append(errs, fmt.Errorf("images.AzureControllers cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.azureControllers %q", i.AzureControllers))
 	}
 
 	if i.PrometheusOperatorBase == "" {
-		errs = append(errs, fmt.Errorf("images.PrometheusOperatorBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.prometheusOperatorBase %q", i.PrometheusOperatorBase))
 	}
 
 	if i.PrometheusBase == "" {
-		errs = append(errs, fmt.Errorf("images.PrometheusBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.prometheusBase %q", i.PrometheusBase))
 	}
 
 	if i.PrometheusConfigReloaderBase == "" {
-		errs = append(errs, fmt.Errorf("images.PrometheusConfigReloaderBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.prometheusConfigReloaderBase %q", i.PrometheusConfigReloaderBase))
 	}
 
 	if i.ConfigReloaderBase == "" {
-		errs = append(errs, fmt.Errorf("images.ConfigReloaderBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.configReloaderBase %q", i.ConfigReloaderBase))
 	}
 
 	if i.AlertManagerBase == "" {
-		errs = append(errs, fmt.Errorf("images.AlertManagerBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.alertManagerBase %q", i.AlertManagerBase))
 	}
 
 	if i.NodeExporterBase == "" {
-		errs = append(errs, fmt.Errorf("images.NodeExporterBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.nodeExporterBase %q", i.NodeExporterBase))
 	}
 
 	if i.GrafanaBase == "" {
-		errs = append(errs, fmt.Errorf("images.GrafanaBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.grafanaBase %q", i.GrafanaBase))
 	}
 
 	if i.KubeStateMetricsBase == "" {
-		errs = append(errs, fmt.Errorf("images.KubeStateMetricsBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.kubeStateMetricsBase %q", i.KubeStateMetricsBase))
 	}
 
 	if i.KubeRbacProxyBase == "" {
-		errs = append(errs, fmt.Errorf("images.KubeRbacProxyBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.kubeRbacProxyBase %q", i.KubeRbacProxyBase))
 	}
 
 	if i.OAuthProxyBase == "" {
-		errs = append(errs, fmt.Errorf("images.OAuthProxyBase cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.oAuthProxyBase %q", i.OAuthProxyBase))
 	}
 
 	if i.MasterEtcd == "" {
-		errs = append(errs, fmt.Errorf("images.MasterEtcd cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.masterEtcd %q", i.MasterEtcd))
 	}
 
 	if i.ControlPlane == "" {
-		errs = append(errs, fmt.Errorf("images.ControlPlane cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.controlPlane %q", i.ControlPlane))
 	}
 
 	if i.Node == "" {
-		errs = append(errs, fmt.Errorf("images.Node cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.node %q", i.Node))
 	}
 
 	if i.ServiceCatalog == "" {
-		errs = append(errs, fmt.Errorf("images.ServiceCatalog cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.serviceCatalog %q", i.ServiceCatalog))
 	}
 
 	if i.Sync == "" {
-		errs = append(errs, fmt.Errorf("images.Sync cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.sync %q", i.Sync))
 	}
 
 	if i.Startup == "" {
-		errs = append(errs, fmt.Errorf("images.Startup cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.startup %q", i.Startup))
 	}
 
 	if i.TemplateServiceBroker == "" {
-		errs = append(errs, fmt.Errorf("images.TemplateServiceBroker cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.templateServiceBroker %q", i.TemplateServiceBroker))
 	}
 
 	if i.Registry == "" {
-		errs = append(errs, fmt.Errorf("images.Registry cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.registry %q", i.Registry))
 	}
 
 	if i.Router == "" {
-		errs = append(errs, fmt.Errorf("images.Router cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.router %q", i.Router))
 	}
 
 	if i.RegistryConsole == "" {
-		errs = append(errs, fmt.Errorf("images.RegistryConsole cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.registryConsole %q", i.RegistryConsole))
 	}
 
 	if i.AnsibleServiceBroker == "" {
-		errs = append(errs, fmt.Errorf("images.AnsibleServiceBroker cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.ansibleServiceBroker %q", i.AnsibleServiceBroker))
 	}
 
 	if i.WebConsole == "" {
-		errs = append(errs, fmt.Errorf("images.WebConsole cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.webConsole %q", i.WebConsole))
 	}
 
 	if i.Console == "" {
-		errs = append(errs, fmt.Errorf("images.Console cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.console %q", i.Console))
 	}
 
 	if i.EtcdBackup == "" {
-		errs = append(errs, fmt.Errorf("images.EtcdBackup cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.etcdBackup %q", i.EtcdBackup))
 	}
 
 	if len(i.GenevaImagePullSecret) == 0 {
-		errs = append(errs, fmt.Errorf("images.GenevaImagePullSecret cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.genevaImagePullSecret %q", i.GenevaImagePullSecret))
 	}
 
 	if i.GenevaLogging == "" {
-		errs = append(errs, fmt.Errorf("images.GenevaLogging cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.genevaLogging %q", i.GenevaLogging))
 	}
 
 	if i.GenevaTDAgent == "" {
-		errs = append(errs, fmt.Errorf("images.GenevaTDAgent cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.genevaTDAgent %q", i.GenevaTDAgent))
 	}
 
 	if i.GenevaStatsd == "" {
-		errs = append(errs, fmt.Errorf("images.GenevaStatsd cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.genevaStatsd %q", i.GenevaStatsd))
 	}
 
 	if i.MetricsBridge == "" {
-		errs = append(errs, fmt.Errorf("images.MetricsBridge cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.metricsBridge %q", i.MetricsBridge))
 	}
 
 	if len(i.ImagePullSecret) == 0 {
-		errs = append(errs, fmt.Errorf("images.ImagePullSecret cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid images.imagePullSecret %q", i.ImagePullSecret))
 	}
 
 	return
@@ -237,23 +237,23 @@ func (v *PluginAPIValidator) validateCertificateConfig(c *pluginapi.CertificateC
 	}
 
 	if c.GenevaLogging.Key == nil {
-		errs = append(errs, fmt.Errorf("GenevaLogging key cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid certificates.genevaLogging.key"))
 	} else if err := c.GenevaLogging.Key.Validate(); err != nil {
-		errs = append(errs, fmt.Errorf("invalid GenevaLogging key: %v", err))
+		errs = append(errs, fmt.Errorf("invalid certificates.genevaLogging.key: %v", err))
 	}
 
 	if c.GenevaLogging.Cert == nil {
-		errs = append(errs, fmt.Errorf("GenevaLogging certificate cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid certificates.genevaLogging.cert"))
 	}
 
 	if c.GenevaMetrics.Key == nil {
-		errs = append(errs, fmt.Errorf("GenevaMetrics key cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid certificates.genevaMetrics.key"))
 	} else if err := c.GenevaMetrics.Key.Validate(); err != nil {
-		errs = append(errs, fmt.Errorf("invalid GenevaMetrics key: %v", err))
+		errs = append(errs, fmt.Errorf("invalid certificates.genevaMetrics.key: %v", err))
 	}
 
 	if c.GenevaMetrics.Cert == nil {
-		errs = append(errs, fmt.Errorf("GenevaMetrics certificate cannot be empty"))
+		errs = append(errs, fmt.Errorf("invalid certificates.genevaMetrics.cert"))
 	}
 
 	return
