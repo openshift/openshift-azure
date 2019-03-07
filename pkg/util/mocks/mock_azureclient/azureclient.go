ced3fb3a68fed0fbc7fc969375cbbb10d827751e
Jim Minter <jminter@redhat.com>
2019-03-06 18:15:49 -0600

    remove ListByResourceGroup from azureclient DNS API

--- pkg/util/mocks/mock_azureclient/azureclient.go
+++ pkg/util/mocks/mock_azureclient/azureclient.go
@@ -756,21 +756,6 @@ func (mr *MockZonesClientMockRecorder) Delete(arg0, arg1, arg2, arg3 interface{}
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Delete", reflect.TypeOf((*MockZonesClient)(nil).Delete), arg0, arg1, arg2, arg3)
 }
 
-// ListByResourceGroup mocks base method
-func (m *MockZonesClient) ListByResourceGroup(arg0 context.Context, arg1 string, arg2 *int32) ([]dns.Zone, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "ListByResourceGroup", arg0, arg1, arg2)
-	ret0, _ := ret[0].([]dns.Zone)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// ListByResourceGroup indicates an expected call of ListByResourceGroup
-func (mr *MockZonesClientMockRecorder) ListByResourceGroup(arg0, arg1, arg2 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ListByResourceGroup", reflect.TypeOf((*MockZonesClient)(nil).ListByResourceGroup), arg0, arg1, arg2)
-}
-
 // MockRecordSetsClient is a mock of RecordSetsClient interface
 type MockRecordSetsClient struct {
 	ctrl     *gomock.Controller
