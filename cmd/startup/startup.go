3719a6eaeaab1aa946e207498501bae4115b0739
Jim Minter <jminter@redhat.com>
2019-03-13 06:15:02 -0600

    move writeTemplatedFiles to pkg/arm

--- cmd/startup/startup.go
+++ cmd/startup/startup.go
@@ -3,11 +3,6 @@ package main
 import (
 	"context"
 	"flag"
-	"io/ioutil"
-	"net"
-	"os"
-	"path"
-	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
@@ -22,7 +17,6 @@ import (
 	"github.com/openshift/openshift-azure/pkg/util/cloudprovider"
 	"github.com/openshift/openshift-azure/pkg/util/configblob"
 	"github.com/openshift/openshift-azure/pkg/util/log"
-	"github.com/openshift/openshift-azure/pkg/util/template"
 	"github.com/openshift/openshift-azure/pkg/util/vault"
 )
 
@@ -36,52 +30,6 @@ type startup struct {
 	cs   *api.OpenShiftManagedCluster
 	kvc  azureclient.KeyVaultClient
 	blob azureclientstorage.Blob
-}
-
-func (s *startup) writeTemplatedFiles() error {
-	hostname, _ := os.Hostname()
-	cname, _ := net.LookupCNAME(hostname)
-	domainname := strings.SplitN(strings.TrimSuffix(cname, "."), ".", 2)[1]
-
-	for _, templateFileName := range arm.AssetNames() {
-		if hostname != "master-000000" && templateFileName == "etc/origin/node/pods/sync.yaml" {
-			continue
-		}
-		if templateFileName == "master-startup.sh" || templateFileName == "node-startup.sh" {
-			continue
-		}
-		s.log.Debugf("processing template %s", templateFileName)
-		templateFile, err := arm.Asset(templateFileName)
-		if err != nil {
-			return errors.Wrapf(err, "Asset(%s)", templateFileName)
-		}
-
-		b, err := template.Template(string(templateFile), nil, s.cs, map[string]interface{}{
-			"Hostname":    hostname,
-			"DNSHostname": domainname,
-		})
-		if err != nil {
-			return errors.Wrapf(err, "Template(%s)", templateFileName)
-		}
-		destination := "/" + templateFileName
-		parentDir := path.Dir(destination)
-		err = os.MkdirAll(parentDir, 0755)
-		if err != nil {
-			return errors.Wrapf(err, "MkdirAll(%s)", parentDir)
-		}
-		var perm os.FileMode = 0666
-		if path.Ext(destination) == ".key" ||
-			path.Ext(destination) == ".kubeconfig" ||
-			path.Base(destination) == "session-secrets.yaml" {
-			perm = 0600
-		}
-
-		err = ioutil.WriteFile(destination, b, perm)
-		if err != nil {
-			return errors.Wrapf(err, "WriteFile(%s)", destination)
-		}
-	}
-	return nil
 }
 
 func (s *startup) initClients(ctx context.Context) error {
@@ -126,7 +74,7 @@ func (s *startup) run(ctx context.Context) error {
 		return errors.Wrap(kerrors.NewAggregate(errs), "can not validate config blob")
 	}
 
-	if err := s.writeTemplatedFiles(); err != nil {
+	if err := arm.WriteTemplatedFiles(s.log, s.cs); err != nil {
 		return errors.Wrap(err, "writeTemplatedFiles")
 	}
 
---
+++ pkg/arm/startup.go
@@ -0,0 +1,61 @@
+package arm
+
+import (
+	"io/ioutil"
+	"net"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+
+	"github.com/openshift/openshift-azure/pkg/api"
+	"github.com/openshift/openshift-azure/pkg/util/template"
+)
+
+func WriteTemplatedFiles(log *logrus.Entry, cs *api.OpenShiftManagedCluster) error {
+	hostname, _ := os.Hostname()
+	cname, _ := net.LookupCNAME(hostname)
+	domainname := strings.SplitN(strings.TrimSuffix(cname, "."), ".", 2)[1]
+
+	for _, templateFileName := range AssetNames() {
+		if hostname != "master-000000" && templateFileName == "etc/origin/node/pods/sync.yaml" {
+			continue
+		}
+		if templateFileName == "master-startup.sh" || templateFileName == "node-startup.sh" {
+			continue
+		}
+		log.Debugf("processing template %s", templateFileName)
+		templateFile, err := Asset(templateFileName)
+		if err != nil {
+			return errors.Wrapf(err, "Asset(%s)", templateFileName)
+		}
+
+		b, err := template.Template(string(templateFile), nil, cs, map[string]interface{}{
+			"Hostname":    hostname,
+			"DNSHostname": domainname,
+		})
+		if err != nil {
+			return errors.Wrapf(err, "Template(%s)", templateFileName)
+		}
+		destination := "/" + templateFileName
+		parentDir := path.Dir(destination)
+		err = os.MkdirAll(parentDir, 0755)
+		if err != nil {
+			return errors.Wrapf(err, "MkdirAll(%s)", parentDir)
+		}
+		var perm os.FileMode = 0666
+		if path.Ext(destination) == ".key" ||
+			path.Ext(destination) == ".kubeconfig" ||
+			path.Base(destination) == "session-secrets.yaml" {
+			perm = 0600
+		}
+
+		err = ioutil.WriteFile(destination, b, perm)
+		if err != nil {
+			return errors.Wrapf(err, "WriteFile(%s)", destination)
+		}
+	}
+	return nil
+}
