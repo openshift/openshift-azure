a5b5b77f00d0d1565b14843e8d6ce2e1970cfa68
Jim Minter <jminter@redhat.com>
2019-03-17 09:43:31 -0600

    add logging for ILB failure flakes

--- test/e2e/standard/cluster.go
+++ test/e2e/standard/cluster.go
@@ -257,6 +257,7 @@ func (sc *SanityChecker) checkCanCreateLB(ctx context.Context) error {
 		By(fmt.Sprintf("waiting for %s to be ready (%v)", lb, time.Now()))
 		err = wait.PollImmediate(2*time.Second, 5*time.Minute, ready.CheckServiceIsReady(sc.Client.EndUser.CoreV1.Services(namespace), lb))
 		if err != nil {
+			sc.Client.EndUser.DumpInfo(namespace)
 			return err
 		}
 	}
