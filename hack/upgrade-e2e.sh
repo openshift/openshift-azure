d772ee745c447937516022df2ac11dd52e626010
Mangirdas <mangirdas@judeikis.lt>
2019-03-25 15:38:00 +0000

    fix upgrade script ordering

--- hack/upgrade-e2e.sh
+++ hack/upgrade-e2e.sh
@@ -1,6 +1,17 @@
 #!/bin/bash -ex
 # upgrade-e2e script is intended to run in CI environmet and will not run localy
 
+### Script flow:
+# 1. Source credentials from secret
+# 2. Create temporary folder for "source cluster" code
+# 3. Start monitoring shim layer from master
+#    We pass config file from temporary folder, 
+#    because it will be created there first during source cluster creation
+# 4. Link secrets to appropriate location
+# 5. Clone released code and create a cluster
+# 6. Switch to PR code base and inicate upgrade.
+#    Old cluster upgarde is always managed by new or same version of RP
+
 if [[ $# -ne 1 ]]; then
     echo error: $0 source_cluster_tag_version
     exit 1
@@ -14,21 +25,22 @@ set +x
 . /usr/local/e2e-secrets/azure/secret
 set -x
 
+T=$(mktemp -d)
+trap "rm -rf $T" EXIT INT
+
 # start monitor from head and record pid
 make monitoring-build
-./monitoring -outputdir=/tmp/artifacts &
+./monitoring -outputdir=/tmp/artifacts -configfile=$T/src/github.com/openshift/openshift-azure/_data/containerservice.yaml &
 MON_PID=$!
 
-T=$(mktemp -d)
 export GOPATH=$T
-trap "rm -rf $T" EXIT
 git clone -b $SOURCE https://github.com/openshift/openshift-azure.git $T/src/github.com/openshift/openshift-azure
 
 cd $T/src/github.com/openshift/openshift-azure
 
-ln -s /usr/local/e2e-secrets/azure secrets
+ln -s /usr/secrets secrets
 
-trap 'kill -15 ${MON_PID}; wait; make delete' EXIT
+trap 'kill -15 ${MON_PID}; wait; make delete' EXIT INT
 
 make create
 
