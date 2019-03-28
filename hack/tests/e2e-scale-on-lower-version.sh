6b0692567cf43121a8ba9355f95884353a6b0bc2
Angus Salkeld <asalkeld@redhat.com>
2019-03-28 14:20:38 +1000

    Add test to prove scaling a v3 cluster on a v4 plugin does not rotate
    closes #1358

---
+++ hack/tests/e2e-scale-on-lower-version.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -ex
+
+if [[ $# -ne 1 ]]; then
+    echo "usage: $0 source_version"
+    exit 1
+fi
+
+cleanup() {
+    set +e
+
+    stop_monitoring
+    make artifacts
+    make delete
+    az group delete -g "$RESOURCEGROUP" --yes --no-wait
+
+    if [[ -n "$T" ]]; then
+        rm -rf "$T"
+    fi
+}
+trap cleanup EXIT
+
+. hack/tests/ci-operator-prepare.sh
+
+T="$(mktemp -d)"
+start_monitoring $T/src/github.com/openshift/openshift-azure/_data/containerservice.yaml
+
+git clone -b "$1" https://github.com/openshift/openshift-azure.git $T/src/github.com/openshift/openshift-azure
+(
+    cd "$T/src/github.com/openshift/openshift-azure"
+    setup_secrets
+    GOPATH="$T" make create
+)
+
+cp -a "$T/src/github.com/openshift/openshift-azure/_data" .
+
+set_build_images
+
+ADMIN_MANIFEST=test/manifests/fakerp/admin-update.yaml make e2e-scaleupdown
