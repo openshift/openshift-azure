b528c0e7f104eced726977d4e3cb9bc247a0c582
Jorge Gallegos <jgallego@redhat.com>
2019-04-01 22:18:21 -0600

    Simplify this even more

--- pkg/vmimage/vmimage_test.go
+++ pkg/vmimage/vmimage_test.go
@@ -1,36 +1,18 @@
 package vmimage
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"os"
 	"testing"
-	"time"
 
 	"github.com/openshift/openshift-azure/test/util/tls"
 )
 
 func TestGenerateTemplate(t *testing.T) {
-	timestamp := time.Now().UTC().Format("200601021504")
-	image := "rhel7-3.11-" + timestamp
-	sshKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	if err != nil {
-		t.Error(err)
-	}
-
 	builder := Builder{
-		SubscriptionID:      os.Getenv("AZURE_SUBSCRIPTION_ID"),
-		Location:            os.Getenv("AZURE_REGION"),
-		BuildResourceGroup:  "imagebuilder",
-		Image:               image,
-		ImageResourceGroup:  "images",
-		ImageStorageAccount: "openshiftimages",
-		ImageContainer:      "images",
-		SSHKey:              sshKey,
-		ClientKey:           tls.GetDummyPrivateKey(),
-		ClientCert:          tls.GetDummyCertificate(),
+		SSHKey:     tls.GetDummyPrivateKey(),
+		ClientKey:  tls.GetDummyPrivateKey(),
+		ClientCert: tls.GetDummyCertificate(),
 	}
-	_, err = builder.generateTemplate()
+	_, err := builder.generateTemplate()
 	if err != nil {
 		t.Error(err)
 	}
