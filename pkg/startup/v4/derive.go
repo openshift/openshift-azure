5c2cb5b75d22c1fa77602b9abaa27df5e1110b3b
Jim Minter <jminter@redhat.com>
2019-04-05 16:04:45 -0600

    switch on UseInstanceMetadata - should significantly reduce Azure API calls and increase reliability

--- pkg/startup/v4/derive.go
+++ pkg/startup/v4/derive.go
@@ -63,11 +63,11 @@ func (derivedType) KubeReserved(cs *api.OpenShiftManagedCluster, role api.AgentP
 }
 
 func (derivedType) MasterCloudProviderConf(cs *api.OpenShiftManagedCluster) ([]byte, error) {
-	return derivedpkg.MasterCloudProviderConf(cs)
+	return derivedpkg.MasterCloudProviderConf(cs, false)
 }
 
 func (derivedType) WorkerCloudProviderConf(cs *api.OpenShiftManagedCluster) ([]byte, error) {
-	return derivedpkg.WorkerCloudProviderConf(cs)
+	return derivedpkg.WorkerCloudProviderConf(cs, false)
 }
 
 // MaxDataDisksPerVM is a stopgap until k8s 1.12.  It requires that a cluster
