91f589cf86f8ba4269ebdf6821a0e1e999cb440b
Mangirdas <mangirdas@judeikis.lt>
2019-04-11 09:45:54 +0100

    rework

--- test/reporters/logger.go
+++
@@ -1,7 +0,0 @@
-package reporters
-
-// GinkgoLogger suppresses logging unless "-v" is given to the gingko framework.
-type AzureLogger struct{}
-
-// GinkgoLogger implements Logger.
-//var _ logrus.StdLogger = &AzureLogger{}
