cbca738e2588e6a9d414f7d05b4c3268cc2ec506
Jim Minter <jminter@redhat.com>
2019-04-29 14:29:35 -0500

    minor fixes/reorders in handleDelete

--- pkg/fakerp/customer_handlers.go
+++ pkg/fakerp/customer_handlers.go
@@ -4,11 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
-
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/azure"
 
 	internalapi "github.com/openshift/openshift-azure/pkg/api"
 	v20190430 "github.com/openshift/openshift-azure/pkg/api/2019-04-30"
@@ -20,35 +16,36 @@ import (
 func (s *Server) handleDelete(w http.ResponseWriter, req *http.Request) {
 	cs := req.Context().Value(ContainerService).(*internalapi.OpenShiftManagedCluster)
 
+	cs.Properties.ProvisioningState = internalapi.Deleting
+	s.store.Put(ContainerServiceKey, cs)
+
+	s.log.Info("deleting service principals")
 	am, err := newAADManager(req.Context(), s.log, cs, s.testConfig)
 	if err != nil {
 		s.badRequest(w, fmt.Sprintf("Failed to delete service principals: %v", err))
 		return
 	}
 
-	s.log.Info("deleting service principals")
 	err = am.deleteApps(req.Context())
 	if err != nil {
 		s.badRequest(w, fmt.Sprintf("Failed to delete service principals: %v", err))
 		return
 	}
 
-	// delete dns records
-	// TODO: get resource group from request path
-	dm, err := newDNSManager(req.Context(), s.log, os.Getenv("AZURE_SUBSCRIPTION_ID"), os.Getenv("DNS_RESOURCEGROUP"), os.Getenv("DNS_DOMAIN"))
+	s.log.Info("deleting dns records")
+	dm, err := newDNSManager(req.Context(), s.log, cs.Properties.AzProfile.SubscriptionID, os.Getenv("DNS_RESOURCEGROUP"), os.Getenv("DNS_DOMAIN"))
 	if err != nil {
 		s.badRequest(w, fmt.Sprintf("Failed to delete dns records: %v", err))
 		return
 	}
+
 	err = dm.deleteOCPDNS(req.Context(), cs)
 	if err != nil {
 		s.badRequest(w, fmt.Sprintf("Failed to delete dns records: %v", err))
 		return
 	}
 
-	resourceGroup := filepath.Base(req.URL.Path)
-	s.log.Infof("deleting resource group %s", resourceGroup)
-
+	s.log.Infof("deleting resource group")
 	authorizer, err := azureclient.GetAuthorizerFromContext(req.Context(), internalapi.ContextKeyClientAuthorizer)
 	if err != nil {
 		s.badRequest(w, fmt.Sprintf("Failed to determine request credentials: %v", err))
@@ -56,22 +53,13 @@ func (s *Server) handleDelete(w http.ResponseWriter, req *http.Request) {
 	}
 
 	gc := resources.NewGroupsClient(req.Context(), s.log, cs.Properties.AzProfile.SubscriptionID, authorizer)
-	err = gc.Delete(req.Context(), resourceGroup)
+	err = gc.Delete(req.Context(), cs.Properties.AzProfile.ResourceGroup)
 	if err != nil {
-		if autoRestErr, ok := err.(autorest.DetailedError); ok {
-			if original, ok := autoRestErr.Original.(*azure.RequestError); ok {
-				if original.StatusCode == http.StatusNotFound {
-					return
-				}
-			}
-		}
 		s.badRequest(w, fmt.Sprintf("Failed to delete resource group: %v", err))
 		return
 	}
 
-	cs.Properties.ProvisioningState = internalapi.Deleting
-	s.store.Put(ContainerServiceKey, cs)
-	w.WriteHeader(http.StatusOK)
+	s.store.Delete(ContainerServiceKey)
 }
 
 func (s *Server) handleGet(w http.ResponseWriter, req *http.Request) {
