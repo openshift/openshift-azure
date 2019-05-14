72896f84db2f9d3233cab44ebac630aa7d1c598b
charlesakalugwu <akalugwu@redhat.com>
2019-05-14 15:50:53 +0200

    Add metrics endpoint to sync app

--- pkg/entrypoint/sync/sync.go
+++ pkg/entrypoint/sync/sync.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 
 	"github.com/openshift/openshift-azure/pkg/api"
@@ -98,6 +99,7 @@ func start(cfg *cmdConfig) error {
 
 	mux := &http.ServeMux{}
 	mux.Handle("/healthz/ready", http.HandlerFunc(s.ReadyHandler))
+	mux.Handle("/metrics", promhttp.Handler())
 
 	go http.Serve(l, mux)
 
