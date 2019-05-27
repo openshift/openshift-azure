24cb294efd37fe1daf96073449e4cec3eaa0b110
charlesakalugwu <akalugwu@redhat.com>
2019-05-27 14:57:22 +0200

    Add sync pod metrics instrumentation

--- pkg/entrypoint/sync/sync.go
+++ pkg/entrypoint/sync/sync.go
@@ -3,10 +3,13 @@ package sync
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net"
 	"net/http"
 	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sirupsen/logrus"
 
@@ -20,6 +23,44 @@ import (
 	"github.com/openshift/openshift-azure/pkg/util/configblob"
 	"github.com/openshift/openshift-azure/pkg/util/enrich"
 	"github.com/openshift/openshift-azure/pkg/util/log"
+)
+
+var (
+	infoGauge = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "sync_info",
+			Help: "General information about the sync process.",
+		},
+		[]string{"plugin_version", "image", "period_seconds"},
+	)
+
+	errorsCounter = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "sync_errors_total",
+			Help: "Total number of errors.",
+		},
+	)
+
+	inFlightGauge = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "sync_executions_inflight",
+			Help: "Number of sync executions in progress.",
+		},
+	)
+
+	lastExecutedGauge = promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "sync_last_executed",
+			Help: "The last time a sync was run.",
+		},
+	)
+
+	durationSummary = promauto.NewSummary(
+		prometheus.SummaryOpts{
+			Name: "sync_duration_seconds",
+			Help: "The duration of sync runs.",
+		},
+	)
 )
 
 func start(cfg *cmdConfig) error {
@@ -70,6 +111,7 @@ func start(cfg *cmdConfig) error {
 		return err
 	}
 	log.Printf("running sync for plugin %s", cs.Config.PluginVersion)
+	infoGauge.With(prometheus.Labels{"plugin_version": cs.Config.PluginVersion, "image": cs.Config.Images.Sync, "period_seconds": fmt.Sprintf("%f", cfg.interval.Seconds())}).Set(1)
 
 	log.Print("enriching config")
 	err = enrich.CertificatesFromVault(ctx, kvc, cs)
@@ -105,12 +147,18 @@ func start(cfg *cmdConfig) error {
 
 	t := time.NewTicker(cfg.interval)
 	for {
+		startTime := time.Now()
 		log.Print("starting sync")
+		inFlightGauge.Inc()
 		if err := s.Sync(ctx); err != nil {
+			errorsCounter.Inc()
 			log.Printf("sync error: %s", err)
 		} else {
 			log.Print("sync done")
 		}
+		durationSummary.Observe(time.Now().Sub(startTime).Seconds())
+		lastExecutedGauge.SetToCurrentTime()
+		inFlightGauge.Dec()
 		if cfg.once {
 			return nil
 		}
