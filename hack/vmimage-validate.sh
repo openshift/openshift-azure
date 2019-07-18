86762fdcfe208e6d4dbc5a737982b6d8c021bf7e
Kenny Woodson <kwoodson@redhat.com>
2019-07-17 23:09:33 -0400

    Adding vmimage validate to vmimage build.

--- hack/vmimage-validate.sh
+++ hack/vmimage-validate.sh
@@ -3,9 +3,7 @@
 cleanup() {
   set +e
 
-  generate_artifacts
-
-  delete
+  az group delete -g "${BUILD_RESOURCE_GROUP}" --yes --no-wait
 }
 
 trap cleanup EXIT
@@ -14,15 +12,17 @@ trap cleanup EXIT
 
 if [ -z "$IMAGE_RESOURCENAME" ] ;
 then
-  IMAGE_RESOURCENAME=$(az image list -g images --query '[-1].name')
+  IMAGE_RESOURCENAME=$(az image list -g images --query '[-1].name' -o tsv)
 fi
 
+BUILD_RESOURCE_GROUP="vmimage-$(date +%Y%m%d%H%M)"
 IMAGE_RESOURCEGROUP="${IMAGE_RESOURCEGROUP:-images}"
 IMAGE_STORAGEACCOUNT="${IMAGE_STORAGEACCOUNT:-openshiftimages}"
 
 echo "Validating: ${IMAGE_RESOURCENAME}"
 
-go generate ./... && go run -ldflags "-X main.gitCommit=$(git rev-parse --short=10 HEAD)" ./cmd/vmimage -imageResourceGroup "${IMAGE_RESOURCEGROUP}" -image "${IMAGE_RESOURCENAME}" -imageStorageAccount "${IMAGE_STORAGEACCOUNT}" -validate "true"
+# pass -preserveBuildResourceGroup so we can copy artifacts
+go generate ./... && go run -ldflags "-X main.gitCommit=$(git rev-parse --short=10 HEAD)" ./cmd/vmimage -buildResourceGroup "${BUILD_RESOURCE_GROUP}" -imageResourceGroup "${IMAGE_RESOURCEGROUP}" -image "${IMAGE_RESOURCENAME}" -imageStorageAccount "${IMAGE_STORAGEACCOUNT}" -validate -preserveBuildResourceGroup
 
 # Currently there are only 3 logs we want to capture
-mv /tmp/{yum_update_info,yum_check_update,scap-report.html} ${ARTIFACTS:-/tmp}/
+mv /tmp/{yum_update_info,yum_check_update,scap-report.html} ${ARTIFACTS:-/tmp}/ || true
