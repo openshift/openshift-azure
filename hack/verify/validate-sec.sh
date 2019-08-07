72c488ed87b1394cb465ac7f39747e8dc7fee763
Elana Hashman <ehashman@redhat.com>
2019-08-07 13:10:53 -0700

    Fix PATH for gosec validation

--- hack/verify/validate-sec.sh
+++ hack/verify/validate-sec.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+
+PATH="$(go env GOPATH)/bin":$PATH
 
 go get github.com/securego/gosec/cmd/gosec
 gosec -severity medium -confidence medium -exclude G304 -quiet  ./...
