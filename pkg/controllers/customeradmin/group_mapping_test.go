8955f3694f4f12f09455a6a88e9b99e902772f72
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2019-09-10 15:16:18 +0100

    add account owner logic

--- pkg/controllers/customeradmin/group_mapping_test.go
+++ pkg/controllers/customeradmin/group_mapping_test.go
@@ -15,35 +15,30 @@ import (
 )
 
 func TestFromMSGraphGroup(t *testing.T) {
+	// see main function for explanations
 	userv1 := fakeuserv1.NewSimpleClientset(
+		// case 1
 		&v1.User{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "live.com#foo@bar.com",
+				Name: "owner@home.com",
 			},
 		},
+		// case 2
 		&v1.User{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "bar@foo.com",
+				Name: "live.com#user@guest.com",
 			},
 		},
+		// case 3
 		&v1.User{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "live.com#user@homecorp.com",
+				Name: "user@trustedGuest.com",
 			},
 		},
+		// case 4
 		&v1.User{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: "user@nativecorp.com",
-			},
-		},
-		&v1.User{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "user@guestcorp.com",
-			},
-		},
-		&v1.User{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: "user@amazingcorp.com",
+				Name: "user@home.com",
 			},
 		},
 	).UserV1()
@@ -154,52 +149,133 @@ func TestFromMSGraphGroup(t *testing.T) {
 			},
 		},
 		{
-			name:          "guest member reconcile external tree",
+			name:          "owner is an admin (case 1)",
+			kubeGroupName: osaCustomerAdmins,
+			want1:         false,
+			kubeGroup: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"owner@home.com"},
+			},
+			msGroupMembers: []graphrbac.User{
+				{
+					Mail:              nil,
+					UserType:          graphrbac.Member,
+					GivenName:         to.StringPtr("owner@home.com"),
+					MailNickname:      to.StringPtr("owner_home.com#EXT#"),
+					UserPrincipalName: to.StringPtr("owner_home.com#EXT#@home2.onmicrosoft.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"owner@home.com"},
+			},
+		},
+		{
+			name:          "guest user with prefix (case 2)",
+			kubeGroupName: osaCustomerAdmins,
+			want1:         false,
+			kubeGroup: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"live.com#user@guest.com"},
+			},
+			msGroupMembers: []graphrbac.User{
+				{
+					Mail:              to.StringPtr("user@guest.com"),
+					UserType:          graphrbac.Guest,
+					GivenName:         nil,
+					MailNickname:      to.StringPtr("user_guest.com#EXT#"),
+					UserPrincipalName: to.StringPtr("user_guest.com#EXT#@home2.onmicrosoft.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"live.com#user@guest.com"},
+			},
+		},
+		{
+			name:          "guest user no with prefix (case 3)",
+			kubeGroupName: osaCustomerAdmins,
+			want1:         false,
+			kubeGroup: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"user@trustedGuest.com"},
+			},
+			msGroupMembers: []graphrbac.User{
+				{
+					Mail:              to.StringPtr("user@trustedGuest.com"),
+					UserType:          graphrbac.Guest,
+					GivenName:         nil,
+					MailNickname:      to.StringPtr("user_trustedGuest.com#EXT#"),
+					UserPrincipalName: to.StringPtr("user_trustedGuest.com#EXT#@home2.onmicrosoft.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"user@trustedGuest.com"},
+			},
+		},
+		{
+			name:          "guest user (case 4)",
+			kubeGroupName: osaCustomerAdmins,
+			want1:         false,
+			kubeGroup: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"user@home.com"},
+			},
+			msGroupMembers: []graphrbac.User{
+				{
+					Mail:              to.StringPtr("user@home.com"),
+					UserType:          graphrbac.Guest,
+					GivenName:         nil,
+					MailNickname:      to.StringPtr("user_home.com#EXT#"),
+					UserPrincipalName: to.StringPtr("user_home.com#EXT#@home2.onmicrosoft.com"),
+				},
+			},
+			want: &v1.Group{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name: osaCustomerAdmins,
+				},
+				Users: []string{"user@home.com"},
+			},
+		},
+		{
+			name:          "leaver user",
 			kubeGroupName: osaCustomerAdmins,
 			want1:         true,
 			kubeGroup: &v1.Group{
 				ObjectMeta: meta_v1.ObjectMeta{
 					Name: osaCustomerAdmins,
 				},
-				Users: []string{"live.com#user@homecorp.com", "user@nativecorp.com", "user@guestcorp.com"},
+				Users: []string{"user@home.com", "leaver@example.com"},
 			},
 			msGroupMembers: []graphrbac.User{
 				{
-					// ocpUser: live.com#user@homecorp.com
-					Mail:              to.StringPtr("user@homecorp.com"),
+					Mail:              to.StringPtr("user@home.com"),
 					UserType:          graphrbac.Guest,
-					MailNickname:      to.StringPtr("user_homecorp.com#EXT#"),
-					UserPrincipalName: to.StringPtr("user_homecorp.com#EXT#@nativecorp.onmicrosoft.com"),
+					GivenName:         nil,
+					MailNickname:      to.StringPtr("user_home.com#EXT#"),
+					UserPrincipalName: to.StringPtr("user_home.com#EXT#@home2.onmicrosoft.com"),
 				},
-				{
-					// ocpUser: user@guestcorp.com
-					Mail:              to.StringPtr("user@guestcorp.com"),
-					UserType:          graphrbac.Guest,
-					MailNickname:      to.StringPtr("user_guestcorp.com#EXT#"),
-					UserPrincipalName: to.StringPtr("user_guestcorp.com#EXT#@nativecorp.onmicrosoft.com"),
-				},
-				{
-					// ocpUser: user@nativecorp.com
-					Mail:              nil,
-					UserType:          graphrbac.Member,
-					MailNickname:      to.StringPtr("user"),
-					UserPrincipalName: to.StringPtr("user@nativecorp.com"),
-				},
-				// TOFIX: This is usecase which I seen, but without customer I cant replicate without
-				// customer help at the moment.
-				//{
-				//	// ocpUser: user@amazingcorp.com
-				//	Mail:              nil,
-				//	UserType:          graphrbac.Guest,
-				//	MailNickname:      to.StringPtr("user@amazingcorp.com"),
-				//	UserPrincipalName: to.StringPtr("user_amazingcorp.com#EXT#@foo.onmicrosoft.com"),
-				//},
 			},
 			want: &v1.Group{
 				ObjectMeta: meta_v1.ObjectMeta{
 					Name: osaCustomerAdmins,
 				},
-				Users: []string{"live.com#user@homecorp.com", "user@guestcorp.com", "user@nativecorp.com"},
+				Users: []string{"user@home.com"},
 			},
 		},
 	}
@@ -209,10 +285,10 @@ func TestFromMSGraphGroup(t *testing.T) {
 			log := logrus.NewEntry(logrus.StandardLogger()).WithField("test", tt.name)
 			got, got1 := fromMSGraphGroup(log, userv1, tt.kubeGroup, tt.kubeGroupName, tt.msGroupMembers)
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("fromMSGraphGroup() got = %v, want %v", got, tt.want)
+				t.Errorf("fromMSGraphGroup()\n got = %v, \nwant %v", got, tt.want)
 			}
 			if got1 != tt.want1 {
-				t.Errorf("fromMSGraphGroup() got1 = %v, want %v", got1, tt.want1)
+				t.Errorf("fromMSGraphGroup()\n got1 = %v, \nwant %v", got1, tt.want1)
 			}
 		})
 	}
---
+++ pkg/util/mail/mail.go
@@ -0,0 +1,9 @@
+package mail
+
+import "regexp"
+
+// Validate validates if email is formated right
+func Validate(email string) bool {
+	Re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return Re.MatchString(email)
+}
