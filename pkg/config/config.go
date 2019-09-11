04a9a86e859dd7e7112dd90ae6ee155262f75918
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2019-09-11 11:40:36 +0100

    add v71 config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -14,6 +14,7 @@ import (
 	v5 "github.com/openshift/openshift-azure/pkg/config/v5"
 	v6 "github.com/openshift/openshift-azure/pkg/config/v6"
 	v7 "github.com/openshift/openshift-azure/pkg/config/v7"
+	v71 "github.com/openshift/openshift-azure/pkg/config/v71"
 )
 
 type Interface interface {
@@ -28,8 +29,10 @@ func New(cs *api.OpenShiftManagedCluster) (Interface, error) {
 		return v5.New(cs), nil
 	case "v6.0":
 		return v6.New(cs), nil
-	case "v7.0", "v7.1":
+	case "v7.0":
 		return v7.New(cs), nil
+	case "v7.1":
+		return v71.New(cs), nil
 	}
 
 	return nil, fmt.Errorf("version %q not found", cs.Config.PluginVersion)
