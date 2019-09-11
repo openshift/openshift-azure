694e7552b1924f572f74cfa746431b36b0a3f762
Charles Akalugwu <akalugwu@redhat.com>
2019-09-11 16:21:44 +0200

    Support multiple AAD reply urls per AAD App

--- pkg/util/mocks/mock_azureclient/mock_graphrbac/graphrbac.go
+++ pkg/util/mocks/mock_azureclient/mock_graphrbac/graphrbac.go
@@ -66,6 +66,21 @@ func (mr *MockApplicationsClientMockRecorder) Delete(arg0, arg1 interface{}) *go
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Delete", reflect.TypeOf((*MockApplicationsClient)(nil).Delete), arg0, arg1)
 }
 
+// Get mocks base method
+func (m *MockApplicationsClient) Get(arg0 context.Context, arg1 string) (graphrbac.Application, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Get", arg0, arg1)
+	ret0, _ := ret[0].(graphrbac.Application)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Get indicates an expected call of Get
+func (mr *MockApplicationsClientMockRecorder) Get(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Get", reflect.TypeOf((*MockApplicationsClient)(nil).Get), arg0, arg1)
+}
+
 // List mocks base method
 func (m *MockApplicationsClient) List(arg0 context.Context, arg1 string) (graphrbac.ApplicationListResultPage, error) {
 	m.ctrl.T.Helper()
