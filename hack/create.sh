dbca6e373a9dd01e24f9c441efc8cc10356992eb
Mangirdas Judeikis <Mangirdas@Judeikis.LT>
2019-10-07 10:20:15 +0100

    add API_VERSION variable

--- hack/create.sh
+++ hack/create.sh
@@ -10,18 +10,19 @@ export RESOURCEGROUP=$1
 rm -rf _data
 mkdir -p _data/_out
 
-if [[ -n "$TEST_IN_PRODUCTION" ]]; then
-    TEST_IN_PRODUCTION="-use-prod=true"
-else
-    [[ -e /var/run/secrets/kubernetes.io ]] || go generate ./...
-    # building takes time, so if we use "go run" we will not get the PID of
-    # the server as it won't have started yet.
-    go build ./cmd/fakerp
-    ./fakerp &
-    trap 'return_id=$?; set +ex; kill $(lsof -t -i :8080); wait $(lsof -t -i :8080); exit $return_id' EXIT
+if [[ -n "$API_VERSION" ]]; then
+    API_VERSION="-api-version=$API_VERSION"
 fi
+
+[[ -e /var/run/secrets/kubernetes.io ]] || go generate ./...
+# building takes time, so if we use "go run" we will not get the PID of
+# the server as it won't have started yet.
+go build ./cmd/fakerp
+./fakerp &
+trap 'return_id=$?; set +ex; kill $(lsof -t -i :8080); wait $(lsof -t -i :8080); exit $return_id' EXIT
+
 if [[ -n "$ADMIN_MANIFEST" ]]; then
     ADMIN_MANIFEST="-admin-manifest=$ADMIN_MANIFEST"
 fi
 
-go run cmd/createorupdate/createorupdate.go ${TEST_IN_PRODUCTION:-} ${ADMIN_MANIFEST:-}
+go run cmd/createorupdate/createorupdate.go ${API_VERSION:-} ${ADMIN_MANIFEST:-}
