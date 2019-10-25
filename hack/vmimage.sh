9c6e7b10d874aa807d6f2c90319e1e719bd0c6ee
Olga Mirensky <olga.mirensky@redhat.com>
2019-10-25 16:10:39 +1100

    Pin image location in vmimage test
    make sure AZURE_REGIONS always contains AZURE_REGION

--- hack/vmimage.sh
+++ hack/vmimage.sh
@@ -23,10 +23,12 @@ fi
 export IMAGE_RESOURCEGROUP="${IMAGE_RESOURCEGROUP:-images}"
 export IMAGE_RESOURCENAME="${IMAGE_RESOURCENAME:-rhel7-3.11-$(TZ=Etc/UTC date +%Y%m%d%H%M)}"
 export IMAGE_STORAGEACCOUNT="${IMAGE_STORAGEACCOUNT:-openshiftimages}"
+export AZURE_REGION="${AZURE_REGION:-eastus}"
+export AZURE_REGIONS="${AZURE_REGION}"
 
 [[ -e /var/run/secrets/kubernetes.io ]] || go generate ./...
 PHASE=image_build
-go run -ldflags "-X main.gitCommit=$GITCOMMIT" ./cmd/vmimage -imageResourceGroup "$IMAGE_RESOURCEGROUP" -image "$IMAGE_RESOURCENAME" -imageStorageAccount "$IMAGE_STORAGEACCOUNT"
+go run -ldflags "-X main.gitCommit=$GITCOMMIT" ./cmd/vmimage -imageResourceGroup "$IMAGE_RESOURCEGROUP" -image "$IMAGE_RESOURCENAME" -imageStorageAccount "$IMAGE_STORAGEACCOUNT" -location "$AZURE_REGION"
 
 if [[ -z "$RESOURCEGROUP" ]]; then
   export RESOURCEGROUP="${IMAGE_RESOURCENAME//./}-e2e"
