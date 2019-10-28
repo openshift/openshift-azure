6f5bdf09d7a0fea5f1f5ba200ca7982fb0dc7d78
Angus Salkeld <asalkeld@redhat.com>
2019-10-28 18:35:50 +1000

    workaround for privatelink: make the ilb probe http

--- pkg/arm/v11/resources.go
+++ pkg/arm/v11/resources.go
@@ -315,11 +315,10 @@ func (g *simpleGenerator) ilbAPIServer() *network.LoadBalancer {
 			Probes: &[]network.Probe{
 				{
 					ProbePropertiesFormat: &network.ProbePropertiesFormat{
-						Protocol:          network.ProbeProtocolHTTPS,
+						Protocol:          network.ProbeProtocolTCP,
 						Port:              to.Int32Ptr(443),
 						IntervalInSeconds: to.Int32Ptr(5),
 						NumberOfProbes:    to.Int32Ptr(2),
-						RequestPath:       to.StringPtr("/healthz"),
 					},
 					Name: to.StringPtr(armconst.LbAPIServerProbeName),
 				},
