f4ef13fd7b24d1565cd6004476d5e98e851c289b
Mikalai Radchuk <mradchuk@redhat.com>
2019-12-12 15:16:13 +0000

    Fixes breaking changes

--- pkg/util/azureclient/network/privatelink/version.go
+++ pkg/util/azureclient/network/privatelink/version.go
@@ -1,10 +1,10 @@
 package privatelink
 
-import "github.com/Azure/go-autorest/version"
+import "github.com/Azure/go-autorest/autorest"
 
 // UserAgent returns the UserAgent string to use when sending http.Requests.
 func UserAgent() string {
-	return "Azure-SDK-For-Go/" + version.Number + " network/2019-06-01"
+	return "Azure-SDK-For-Go/" + autorest.Version() + " network/2019-06-01"
 }
 
 // Version returns the semantic version (see http://semver.org) of the client.
